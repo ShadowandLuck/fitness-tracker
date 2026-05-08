c29e7dfa182ea30f008ca24f5cf2bd72eadc39bf
ShadowandLuck <144648496+ShadowandLuck@users.noreply.github.com>
2026-05-08 06:32:54 -0700

    Initial release
    Initial release

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'ft-v1';
+const ASSETS = ['/', '/fitness-tracker/', '/fitness-tracker/index.html',
+  '/fitness-tracker/manifest.json', '/fitness-tracker/icon-192.png', '/fitness-tracker/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  if (e.request.url.includes('googleapis.com') || e.request.url.includes('accounts.google.com')) return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
